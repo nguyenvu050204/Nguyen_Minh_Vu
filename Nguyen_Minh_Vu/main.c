c2173f71476b1e6e37f1305c10ff341ebf6bb764
nguyenvu050204 <nguyenminhvi1010@gmail.com>
2025-03-16 10:04:37 +0700

    thay doi

--- Nguyen_Minh_Vu/main.c
+++ Nguyen_Minh_Vu/main.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 
 void main() {
+	/*
 	int count = 0;
 	char srt[] = "hello";
 	while (srt[count] != NULL) {
 		count++;
 	}
 	printf("%d", count);
-
+	*/
+	char str[] = "hello";
+	int i;
+	for (i = 0; str[i] != NULL;i++) {
+		if (str[i] >= 97 && str[i] <= 122) {
+			str[i] = str[i] - (97 - 65);
+		}
+	}
+	printf("%s",str);
 }
